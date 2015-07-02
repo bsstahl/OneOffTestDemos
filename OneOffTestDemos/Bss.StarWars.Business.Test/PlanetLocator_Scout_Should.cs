3d7337a3c428468e0190b998f89acabaec2909d1
bsstahl <barry@bsstahl.com>
2015-07-01 21:39:09 -0700

    Setup Demo Projects

---
+++ OneOffTestDemos/Bss.StarWars.Business.Test/PlanetLocator_Scout_Should.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bss.StarWars.Business.Test
+{
+    [TestClass]
+    public class PlanetLocator_Scout_Should
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ OneOffTestDemos/LibraryUnderTest/PlanetLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bss.StarWars.Business
+{
+    public class PlanetLocator
+    {
+        public IEnumerable<Planet> Scout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
