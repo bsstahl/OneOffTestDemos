1676316dbabfa6a865a30ad1cb77000852243c86
bsstahl <barry@bsstahl.com>
2015-07-13 13:34:08 -0700

    OnlyReturnPlanetsWithOneStandardGravityForHumans - Red Verification - Additional Files

---
+++ OneOffTestDemos/Bss.StarWars/SpeciesName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bss.StarWars
+{
+    public enum SpeciesName
+    {
+        Unknown,
+        Human,
+        Droid,
+        Rodian,
+        Hutt,
+        Trandoshan
+    }
+}
