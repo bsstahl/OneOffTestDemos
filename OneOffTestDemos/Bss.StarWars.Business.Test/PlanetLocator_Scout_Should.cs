d2022b7200da424b4b05fc72edabbd6c9cf45909
bsstahl <barry@bsstahl.com>
2015-08-26 14:07:43 -0700

    Actually removed ReturnTheCorrectNumberOfPlanetsWithOneGravity this time.

--- OneOffTestDemos/Bss.StarWars.Business.Test/PlanetLocator_Scout_Should.cs
+++ OneOffTestDemos/Bss.StarWars.Business.Test/PlanetLocator_Scout_Should.cs
@@ -14,34 +14,6 @@ namespace Bss.StarWars.Business.Test
 
 
         [TestMethod]
-        public void ReturnTheCorrectNumberOfPlanetsWithOneGravity()
-        {
-            var mocks = new Rhino.Mocks.MockRepository();
-            var db = (null as IPlanetRepository).GetRepository(mocks);
-
-            var allPlanets = new List<Planet>();
-            allPlanets.Add((null as Planet).Create(0.5f));
-            allPlanets.Add((null as Planet).Create(1));
-            allPlanets.Add((null as Planet).Create(0.9f));
-            allPlanets.Add((null as Planet).Create(1));
-            allPlanets.Add((null as Planet).Create(1.1f));
-
-            Rhino.Mocks
-                .Expect.Call(db.GetAllPlanets())
-                .Repeat.Any()
-                .Return(allPlanets);
-
-            mocks.ReplayAll();
-
-            var target = new Bss.StarWars.Business.PlanetLocator(db);
-            var actual = target.Scout();
-
-            Assert.AreEqual(2, actual.Count());
-
-            mocks.VerifyAll();
-        }
-
-        [TestMethod]
         public void ReturnOnlyPlanetsWithGravityGreaterThan95PercentOfStandard()
         {
             var mocks = new Rhino.Mocks.MockRepository();
