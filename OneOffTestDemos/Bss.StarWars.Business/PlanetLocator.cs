03cb94204f20a98c337aa912f4da81ec4a1e0e5f
bsstahl <barry@bsstahl.com>
2015-08-27 22:49:10 -0700

    PlanetLocator_Scout_Should.ReturnOnlyPlanetsWithPopulationGreaterThan10K - Green.

--- OneOffTestDemos/Bss.StarWars.Business/PlanetLocator.cs
+++ OneOffTestDemos/Bss.StarWars.Business/PlanetLocator.cs
@@ -10,6 +10,7 @@ namespace Bss.StarWars.Business
     {
         const Single _minGravityForScout = 0.95f;
         const Single _maxGravityForScout = 1.05f;
+        const int _minPopulationForScout = 10000;
 
         IPlanetRepository _planetRepository;
         public PlanetLocator(IPlanetRepository planetRepository)
@@ -20,7 +21,8 @@ namespace Bss.StarWars.Business
         public IEnumerable<Planet> Scout()
         {
             return _planetRepository.GetAllPlanets()
-                .Where(p => p.Gravity > _minGravityForScout && p.Gravity < _maxGravityForScout);
+                .Where(p => p.Gravity > _minGravityForScout && p.Gravity < _maxGravityForScout)
+                .Where(p => p.Population > _minPopulationForScout);
         }
     }
 }
